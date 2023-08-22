c300079d4f9cc148a3511018f1038d6a4b06ab89
samiunique <yitesam@gmail.com>
2023-08-22 14:06:44 -0700

    update prototype and header file to handle i

--- main.h
+++ main.h
@@ -18,8 +18,9 @@ typedef struct convert f_specifier;
 int parser(const char *format, f_specifier specifiers[], va_list arg_list);
 int _printf(const char *format, ...);
 
+int print_number(va_list args);
 int handle_c(va_list);
 int handle_s(va_list);
 int handle_per();
-
+int handle_i(va_list list);
 #endif
