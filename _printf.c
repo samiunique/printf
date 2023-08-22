c8681e41b8f3d209a52aad6044cc1fb75ac86054
samiunique <yitesam@gmail.com>
2023-08-22 12:29:43 -0700

    create header parser and format file

---
+++ _printf.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  _printf - function to creat printf list formats
+ *  @format: format of specifiers
+ *
+ *  Return: out put to print
+ */
+
+int _printf(const char *format, ...)
+{
+	int print_out;
+	f_specifier specifiers[] = {
+		{"c", handle_c},
+		{"s", handle_s},
+		{"%", handle_per},
+		{NULL, NULL}
+	};
+	va_list arg_list;
+
+	if (format == NULL)
+		return (-1);
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	print_out = parser(format, specifiers, arg_list);
+	va_end(arg_list);
+	return (print_out);
+}
---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef PRINT_F
+#define PRINT_F
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+struct convert
+{
+        char *type;
+        int (*f)(va_list);
+};
+typedef struct convert f_specifier;
+
+/* functions*/
+int parser(const char *format, f_specifier specifiers[], va_list arg_list);
+int _printf(const char *format, ...);
+
+int handle_c(va_list);
+int handle_s(va_list);
+int handle_per();
+
+#endif
