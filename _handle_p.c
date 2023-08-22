70e7e70056cc25e62ca1cb60bd3a373718c4fe79
samiunique <yitesam@gmail.com>
2023-08-22 16:23:53 -0700

    betty forbids un used va and ()

--- _handle_p.c
+++ _handle_p.c
@@ -3,9 +3,10 @@
 /**
  * handle_per - Prints a percent symbol
  *
+ * @list: unused
  * Return: return the amount of characters printed.
  */
-int  handle_per()
+int handle_per(__attribute__((unused))va_list list)
 {
 	putchar('%');
 	return (1);
