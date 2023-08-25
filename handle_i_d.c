cc40a54346c586bd464ef2f126aa6d637389308c
samiunique <yitesam@gmail.com>
2023-08-25 11:32:16 -0700

    add file

---
+++ handle_i_d.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * handle_i - Handles %i format specifier
+ * @list: List of arguments
+ * Return: Number of characters printed
+ */
+int handle_i(va_list list)
+{
+    int num_length;
+    num_length = print_number(list);
+    return (num_length);
+}
+
+/**
+ * handle_d - Handles %d format specifier (same as %i)
+ * @list: List of arguments
+ * Return: Number of characters printed
+ */
+int handle_d(va_list list)
+{
+    int num_length;
+    num_length = print_number(list);
+    return (num_length);
+}
